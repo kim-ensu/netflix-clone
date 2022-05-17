4ad9f98489b485f0b62b1ca34972db51b98c6b1e
dkimd5 <dkimd5@mail.ru>
2022-05-17 12:00:56 +0300

    Add main img and overlay

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,21 @@ function Main() {
 
   console.log(movie);
 
-  return <div>Main</div>;
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+        <div>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
