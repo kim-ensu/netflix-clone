bdec5f258316c006167df11628b64a441226be29
dkimd5 <dkimd5@mail.ru>
2022-05-17 14:42:13 +0300

    Add map for movie images

--- src/components/Row.js
+++ src/components/Row.js
@@ -16,7 +16,16 @@ function Row({ title, fetchURL }) {
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center">
-        <div id={"slider"}></div>
+        <div id={"slider"}>
+          {movies.map((item, id) => (
+            <div className="w-[160px] sm:w=[200px] md:w=[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+                alt={item?.title}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
