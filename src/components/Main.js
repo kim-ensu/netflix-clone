dd58aac918c3a532158f2dff7abac8366eaac195
dkimd5 <dkimd5@mail.ru>
2022-05-17 11:48:44 +0300

    Add random movie display algorithm

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,19 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import requests from "../Requests";
 
 function Main() {
   const [movies, setMovies] = useState([]);
 
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
   useEffect(() => {
     axios.get(requests.requestPopular).then((response) => {
-      setMovies(response.data);
+      setMovies(response.data.results);
     });
   }, []);
 
-  console.log(movies);
+  console.log(movie);
 
   return <div>Main</div>;
 }
