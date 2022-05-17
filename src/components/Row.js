d9781ec380ed1f2a67c07a47b115d8b66d79c566
dkimd5 <dkimd5@mail.ru>
2022-05-17 14:04:32 +0300

    Add data request for row

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function Row() {
-  return <div>Row</div>;
+function Row(title, fetchURL) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  console.log(movies);
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        <div id={"slider"}></div>
+      </div>
+    </>
+  );
 }
 
 export default Row;
