9d7bf19b2c9f1e7d88881e2400d76ce76976b684
dkimd5 <dkimd5@mail.ru>
2022-05-18 11:29:34 +0300

    Add scroll left and right buttons

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
@@ -16,7 +17,11 @@ function Row({ title, fetchURL }) {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          size={40}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
         <div
           id={"slider"}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
@@ -24,6 +29,10 @@ function Row({ title, fetchURL }) {
             <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          size={40}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
       </div>
     </>
   );
