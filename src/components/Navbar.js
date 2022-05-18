091b5e53d5739425c52a602572a5eecde0001e8e
dkimd5 <dkimd5@mail.ru>
2022-05-18 20:11:17 +0300

    Add buttons change for loged in users

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 function Navbar() {
+  const { user, logOut } = UserAuth();
+  // console.log(user.email);
+
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
         <h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="text-white pr-4">Sign In</button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer">Sign up</button>
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4">Account</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer">Logout</button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer">Sign up</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
