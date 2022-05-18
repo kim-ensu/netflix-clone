fb150a00ff207dce08e4ac0f4a660f65ceb598a0
dkimd5 <dkimd5@mail.ru>
2022-05-18 11:35:44 +0300

    Add scroll left right button's logic

--- src/components/Row.js
+++ src/components/Row.js
@@ -12,13 +12,22 @@ function Row({ title, fetchURL }) {
     });
   }, [fetchURL]);
 
-  console.log(movies);
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
         <MdChevronLeft
+          onClick={slideLeft}
           size={40}
           className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
         />
@@ -30,6 +39,7 @@ function Row({ title, fetchURL }) {
           ))}
         </div>
         <MdChevronRight
+          onClick={slideRight}
           size={40}
           className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
         />
