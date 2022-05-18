130384c1f8f9399ac518bed21e5289b0e9efe075
dkimd5 <dkimd5@mail.ru>
2022-05-18 11:37:55 +0300

    Add id for rows

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-function Row({ title, fetchURL }) {
+function Row({ title, fetchURL, rowID }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,12 +13,12 @@ function Row({ title, fetchURL }) {
   }, [fetchURL]);
 
   const slideLeft = () => {
-    let slider = document.getElementById("slider");
+    let slider = document.getElementById("slider" + rowID);
     slider.scrollLeft = slider.scrollLeft - 500;
   };
 
   const slideRight = () => {
-    let slider = document.getElementById("slider");
+    let slider = document.getElementById("slider" + rowID);
     slider.scrollLeft = slider.scrollLeft + 500;
   };
 
@@ -32,7 +32,7 @@ function Row({ title, fetchURL }) {
           className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
         />
         <div
-          id={"slider"}
+          id={"slider" + rowID}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
