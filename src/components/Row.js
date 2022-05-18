5c293147e0e518eeb6ecbf6dc97e4d3f102f8f5a
dkimd5 <dkimd5@mail.ru>
2022-05-18 11:24:57 +0300

    Style row with movies

--- src/components/Row.js
+++ src/components/Row.js
@@ -17,7 +17,9 @@ function Row({ title, fetchURL }) {
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center">
-        <div id={"slider"}>
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
