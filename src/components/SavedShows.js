47d9444b4fc3d415d312cb4d6011b1224a74a42b
dkimd5 <dkimd5@mail.ru>
2022-05-19 13:10:33 +0300

    Add delete show from favourites button

--- src/components/SavedShows.js
+++ src/components/SavedShows.js
@@ -3,6 +3,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
 
 function SavedShows() {
   const [movies, setMovies] = useState([]);
@@ -23,6 +24,18 @@ function SavedShows() {
       setMovies(doc.data()?.savedShows);
     });
   }, [user?.email]);
+
+  const movieRef = doc(db, "users", `${user?.email}`);
+  const deleteShow = async (passedID) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedID);
+      await updateDoc(movieRef, {
+        savedShows: result,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -48,6 +61,11 @@ function SavedShows() {
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                   {item?.title}
+                </p>
+                <p
+                  onClick={() => deleteShow(item.id)}
+                  className="absolute text-gray-300 top-4 right-4">
+                  <AiOutlineClose />
                 </p>
               </div>
             </div>
