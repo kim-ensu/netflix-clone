f1fa1a22d7edb18b1f975dcad19d64da783028d6
dkimd5 <dkimd5@mail.ru>
2022-05-19 11:49:22 +0300

    Add error message for invalid password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,16 +5,19 @@ import { UserAuth } from "../context/AuthContext";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const { user, logIn } = UserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
     try {
       await logIn(email, password);
-      // navigate("/");
+      navigate("/");
     } catch (error) {
       console.log(error);
+      setError(error.message);
     }
   };
 
@@ -30,6 +33,7 @@ function Login() {
         <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
           <div className="max-w-[320px] mx-auto py-16">
             <h1 className="text-3xl font-bold">Sign In</h1>
+            {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
             <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
               <input
                 onChange={(e) => setEmail(e.target.value)}
